39569cf376dfa84dc4ff87f6c4c5d17f405dc48c
fahimcsebuet <fahimcsebuet@gmail.com>
2015-11-22 15:34:08 +0600

    Initialize the file for vertex cover algorithm

---
+++ scripts/vertex_cover/vertex_cover.js
@@ -0,0 +1,3 @@
+// implements the vertex cover algorithm
+// \param Takes the edge-list and the node-list as the parameter
+// \return Makes a file in graph folder that has the list of the nodes for a vertex cover
